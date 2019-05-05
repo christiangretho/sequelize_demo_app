54f13c6de9cf8037198d6e76b922a330bbf03357
Christian B <christianbart13y@gmail.com>
2019-05-05 10:46:45 -0400

    added test data and filtered data

--- server.js
+++ server.js
@@ -1,82 +1,94 @@
-const express = require('express');
-const Sequelize = require('sequelize');
+const express = require("express");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+const _USERS = require("./users.json");
 
 const app = express();
 const port = 8001;
 
-const connection = new Sequelize('db', 'user', 'pass', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    storage: 'db.sqlite',
-    operatorsAliases: false,
-    define: {
-        freezeTableName: true
-    }
+const connection = new Sequelize("db", "user", "pass", {
+  host: "localhost",
+  dialect: "sqlite",
+  storage: "db.sqlite",
+  operatorsAliases: false,
+  define: {
+    freezeTableName: true
+  }
 });
 
-const User = connection.define('User', {
-    // attribute qualifiers
-    uuid: {
-        type: Sequelize.UUID,
-        primaryKey: true,
-        defaultValue: Sequelize.UUIDV4
-    },
-    first: Sequelize.STRING,
-    last: Sequelize.STRING,
-    full_name: Sequelize.STRING,
-    bio: Sequelize.TEXT
-    }, {
-        hooks: {
-            beforeValidate: () => {
-                console.log('before validate');
-            },
-            afterValidate: () => {
-                console.log('after validate');
-            },
-            beforeCreate: (user) => {
-                user.full_name = `${user.first} ${user.last}`
-            },
-            afterCreate: () => {
-                console.log('after create');
-            }
-        } 
+const User = connection.define("User", {
+  // attribute qualifiers
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    validate: {
+      isAlphanumeric: true
+    }
+  }
+});
+
+app.get("/findall", (req, res) => {
+  User.findAll({
+      where: {
+          name: {
+              [Op.like]: 'N%'
+          }
+      }
+  })
+    .then(user => {
+      res.json(user);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).send(error);
     });
+});
 
-
-    // app.get('/', (req, res) => {
-    //     User.create({
-    //         name: 'christian',
-    //         bio: 'first bio entry'
-    //     })
-    //     .then(user => {
-    //         res.json(user);
-    //     })
-    //     .catch(error => {
-    //         console.log(error);
-    //         res.status(404).send(error);
-    //     })
-    // })
+// app.post('/post', (req, res) => {
+//     const newUser = req.body.user;
+//     User.create({
+//         name: newUser.name,
+//         email: newUser.email
+//     })
+//     User.create({
+//         name: 'christian',
+//         bio: 'first bio entry'
+//     })
+//     .then(user => {
+//         res.json(user);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//         res.status(404).send(error);
+//     })
+// })
 
 connection
-    .sync({
-        logging: console.log,
-        force: true
-    })
-    .then(()=>{
-        User.create({
-            first: 'christian',
-            last: 'bartley',
-            bio: 'first bio entry'
-        })
-    })
-    .then(()=> {
-        console.log('Connection to database established successfully');
-    })
-    .catch(err => {
-        console.log('Unable to connect to the database: ', err);
-    });
+  .sync({
+    // logging: console.log
+  })
+  // .then(()=>{
+  //     User.bulkCreate(_USERS)
+  //     .then(users => {
+  //         console.log('Success adding users');
+  //     })
+  //     .catch(error => {
+  //         console.log(error);
+  //     })
+  // })
+  .then(() => {
+    console.log("Connection to database established successfully");
+  })
+  .catch(err => {
+    console.log("Unable to connect to the database: ", err);
+  });
 
 app.listen(port, () => {
-    console.log('Running server on port ' + port);
+  console.log("Running server on port " + port);
 });
-
